3f168ab9f7defccf5d928bd783e9c29058f0440e
NarekKHacatryan1009 <132485862+NarekKHacatryan1009@users.noreply.github.com>
2023-08-27 16:03:01 +0400

    Cikles
    Cikles

---
+++ Cikles/js/script.js
@@ -0,0 +1,69 @@
+// cikles
+// 1. while  2. do while  3.for
+ // 1.
+//  while - qani der
+// let i = 0
+//  while(i < 5){
+//     i++
+//     alert("Hello" +i)
+// }
+
+
+// let a = "Narek"
+// while(a.length = 1){
+//     a = a.split()
+//     console.log(a);
+//     alert(a)
+//     a++
+// }
+
+
+// 2.do while - minchev paymani linely
+// let a = 0
+// do{
+//     alert("Narek")
+//     a++
+// } while(a < 5)
+
+
+// let a = "Narek"
+// do{
+//     a = a.split()
+//     alert(a)
+//     console.log(a);
+//     a++
+// }while(a.length == 1)
+
+
+// 3. for 
+// let i = 0;
+// for(;i < 5;){
+//     alert(i)
+//     i++
+// }
+
+
+// let i = 0
+// for(;;){
+//     i++
+//     alert(i)
+// } // anverj cikl
+
+
+// let i = 0
+// for(let i = 0; i < 5; i++){
+//     for(let j = 0; j < 5; j++){
+//         alert(`i${i} ev j${j}`)
+//     }
+// }
+
+
+
+// for(let i = 0; i < 100; i++){
+//     let a = prompt("Greq tiv")
+//     if(a % 2 == 0){
+//         alert(`zuyg tiv`)
+//         break
+//     }else{alert(`kent tiv`)}
+//     break
+// }
